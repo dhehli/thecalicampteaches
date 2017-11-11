16e23d4ebd3e2a50b7baa2ac8ecab1142d1e4f6c
dhehli <dh@netlive.ch>
2017-11-11 14:52:28 +0100

    add basic crud to express

--- server/models/heroes.js
+++ server/models/heroes.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const config  = require('config');
+var r = require('rethinkdbdash')(config.get('rethinkdb'));
 
+const table = 'heroes';
+
+/* GET api listing. */
+router.get('/heroesApi', (req, res) => {
+  r.table(table)
+  .run()
+  .then(response =>	res.json(response))
+  .error(err => res.sendStatus(500))
+});
+
+router.post('/heroesApi', (req, res) => {
+  r.table(table)
+  .run()
+  .
+})
+
+module.exports = router;
