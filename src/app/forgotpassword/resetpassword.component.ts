be4ed7827824c619db638917fae1a023a378a04f
dhehli <dh@netlive.ch>
2017-12-08 21:55:21 +0100

    add password

---
+++ src/app/forgotpassword/resetpassword.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-resetpassword',
+  templateUrl: './resetpassword.component.html'
+})
+export class ResetpasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
