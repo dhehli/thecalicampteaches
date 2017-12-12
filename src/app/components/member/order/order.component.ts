2e3ca8b1b3d956c08c6c22bfb8ecb77ab4155fd3
dhehli <dh@netlive.ch>
2017-12-12 21:30:58 +0100

    add order components

---
+++ src/app/components/member/order/order.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html'
+})
+export class OrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
