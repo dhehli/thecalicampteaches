1e4ec7276d050ad5842baee4f2a502b5ccd5b67d
dhehli <dh@netlive.ch>
2017-12-15 14:45:07 +0100

    add ajax loader

--- src/app/components/admin/testimonial/testimonial-add.component.ts
+++ src/app/components/admin/testimonial/testimonial-add.component.ts
@@ -11,9 +11,10 @@ import { TestimonialService } from './testimonial.service';
 export class TestimonialAddComponent implements OnInit {
   error = {};
   success: boolean;
+  isSubmitting: false;
 
   constructor(
-    private testimonialService: TestimonialService
+    private testimonialService: TestimonialService,
   ) { }
 
   ngOnInit() {
@@ -21,6 +22,7 @@ export class TestimonialAddComponent implements OnInit {
   }
 
   add(firstname: string, lastname: string, quote: string, onlineState: string): void {
+    this.isSubmitting = true;
     let formData = new FormData();
 
     let file = $("#image")[0].files[0];
@@ -34,6 +36,7 @@ export class TestimonialAddComponent implements OnInit {
 
     this.testimonialService.addTestimonial(formData)
     .subscribe(testimonial => {
+      this.isSubmitting = false;
       this.error = [];
 
       if(testimonial.errors){
