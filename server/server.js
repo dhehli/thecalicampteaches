95344241b93b27cf474ffbbe7e5f47f85158c444
dhehli <dh@netlive.ch>
2018-01-09 17:42:16 +0100

    change static path to dist.html

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ app.use('/api', isAdmin, teamAdmin);
  * Catch all other routes and return the index file
  */
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 /**
